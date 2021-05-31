b5bc7b1015a3d1a6ec9cb1ea9ab20dbb9aa66e22
Daniel Schönfeld <50236905+danielschoenfeld@users.noreply.github.com>
2021-05-31 20:12:43 +0200

    Update cookbook.py

--- cookbook.py
+++ cookbook.py
@@ -1,5 +1,10 @@
 print("Hello Docker World")
 
+#Simple Text input from shell
+print("What is your name?")
+input_name = input()
+print(f"Hello {input_name}")
+
 print("60 Punkte IT Infrastrukturen!")
 
 print("Test Docker pipeline")
